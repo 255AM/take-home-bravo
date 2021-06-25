18527267838be10a4f293b132b3735f8d4325bc1
Chance Lane <lanecw@gmail.com>
2021-06-25 18:12:05 -0400

    created functional tables using static dataSet

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+//initialize tables| defining column titless and pointing to dataSet as data source
+$(document).ready(function() {
+    $('#example').DataTable( {
+        data: dataSet,
+        columns: [
+            { title: "Name" },
+            { title: "Position" },
+            { title: "Office" },
+            { title: "Extn." },
+            { title: "Start date" },
+            { title: "Salary" }
+        ]
+    } );
+} );
