94c47d3a909a3d647ab59eec0913c72bd1f20bfc
Chance Lane <lanecw@gmail.com>
2021-06-26 12:51:13 -0400

    changed button to a render opposed to default. Easier to retireve row id needed for edit fc

--- scripts/main.js
+++ scripts/main.js
@@ -25,9 +25,17 @@ function buildTable(){
                 { title: "Start date" },
                 { title: "Salary" },
                 {
-                    "data": null,
-                    "defaultContent": '<button class="edit">Edit</button> <button class="remove">Delete</button>'
+                    sortable: false,
+                    "render": function ( data, type, full, meta ) {
+                        return '<a id='+meta.row+' class="btn btn-primary btn-sm edit" role="button">Edit</a> <a id='+meta.row+' class="btn btn-primary btn-sm remove" role="button">Delete</a>';
+                    }
                 },
+                
+                
+                // {
+                //     "data": null,
+                //     "defaultContent": '<button class="edit">Edit</button> <button class="remove">Delete</button>'
+                // },
             ]
         } );
         //fx to delete row
@@ -44,14 +52,11 @@ function buildTable(){
 
             var elements = document.getElementById("#edit-form").elements;
             let valsa = table.row( $(this).parents('tr') ).data()
-                
+            let rowIndex = (table.row( $(this).parents('tr') )[0]);
             for (var i = 0; i<6 ; i++) {
                 elements[i].value = valsa[i] 
             }
         })
-        
-           
-              
     });
 
     $('#example').on('click', 'dt-button', function () {
@@ -86,4 +91,12 @@ function addData() {
     resetTable()
 };
 
+function editData(){
+    var table = $('#example').DataTable();
+    table.row($(this).attr('rowindex')).data([$("#type").val(), $("#amount").val()]).draw();
+}
 
+document.getElementById("edit-data").addEventListener("click", function(){
+    editData()
+})
+   
