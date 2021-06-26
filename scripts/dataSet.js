49a7f1d7d79844edf9c9551ff4d956676eec10bb
Chance Lane <lanecw@gmail.com>
2021-06-25 21:26:03 -0400

    added edit and delete buttons to each cell of last row

--- scripts/dataSet.js
+++ scripts/dataSet.js
@@ -1,7 +1,7 @@
 //static datatable to poulate table. will add crud capability, followed by an attempt to access real world data without server side architecture
 
 var dataSet = [
-    [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
+    [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800",'<i class="fa-solid fa-pencil"></i>' ],
     [ "Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
     [ "Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000" ],
     [ "Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "6224", "2012/03/29", "$433,060" ],
--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,41 @@
 //initialize tables| defining column titless and pointing to dataSet as data source
-$(document).ready(function() {
+function buildTable(){
+    $(document).ready(function() {
     $('#example').DataTable( {
         data: dataSet,
         columns: [
-            { title: "Name" },
+            { title: "Name"},
             { title: "Position" },
             { title: "Office" },
             { title: "Extn." },
             { title: "Start date" },
-            { title: "Salary" }
+            { title: "Salary" },
+            {
+                "data": null,
+                "defaultContent": "<button>Edit</button> <button>Delete</button>"
+              }
         ]
     } );
 } );
 
-function saveArticle () {
-    dataSet.push([ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ])
-  };
+}
+//initial table build on load
+buildTable()
+function resetTable(){
+    //Destroy and rebuild the Datatable after source data changes
+    $('#example').DataTable().clear().destroy();
+    buildTable()
+}
+//listen for add button to add data to table
+document.getElementById("add-row").addEventListener("click", saveArticle);
 
-  saveArticle()
+function saveArticle () {
+    dataSet.push([ "toe toe Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ])
+    console.log('here');
+    resetTable()
+    console.log(dataSet);
+};
+
+
+
+  
