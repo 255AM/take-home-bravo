790a6c9ff5b1b66fcfb20d4cb81ca78095ed8999
Chance Lane <lanecw@gmail.com>
2021-06-25 23:23:45 -0400

    edit form populates with row data on edit btn press" "

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,44 @@
 //initialize tables| defining column titless and pointing to dataSet as data source
 function buildTable(){
     $(document).ready(function(){
-    $('#example').DataTable( {
-        select: true,
-        data: dataSet,
-        columns: [
-            { title: "Name"},
-            { title: "Position" },
-            { title: "Office" },
-            { title: "Extn." },
-            { title: "Start date" },
-            { title: "Salary" },
-            {
-                "data": null,
-                "defaultContent": '<button>Edit</button> <button class="remove">Delete</button>'
-              },
-        ]
-    } );
-    $('#example').on('click', '.remove', function () {
-		var table = $('#example').DataTable();
-		table
-			.row($(this).parents('tr'))
-			.remove()
-		.draw();
-		});
-    } );
+        $('#example').DataTable( {
+            select: true,
+            data: dataSet,
+            columns: [
+                { title: "Name"},
+                { title: "Position" },
+                { title: "Office" },
+                { title: "Extn." },
+                { title: "Start date" },
+                { title: "Salary" },
+                {
+                    "data": null,
+                    "defaultContent": '<button class="edit">Edit</button> <button class="remove">Delete</button>'
+                },
+            ]
+        } );
+        $('#example').on('click', '.remove', function () {
+            var table = $('#example').DataTable();
+            table
+                .row($(this).parents('tr'))
+                .remove()
+            .draw();
+            });
+            $('#example tbody').on('click', '.edit', function () {
+                var table = $('#example').DataTable();
+
+                var elements = document.getElementById("#edit-form").elements;
+                let valsa = table.row( $(this).parents('tr') ).data()
+                    
+                for (var i = 0; i<5 ; i++) {
+                    elements[i].value = valsa[i] 
+                }
+            })
+    });
+    
+
+    
+    
 
 }
 //initial table build on load
